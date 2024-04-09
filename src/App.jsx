e5d9d325b0e0f65950f38541f16ecf1fa429617a
kuldeepsikarwar8596 <ksikarwar8596@gmail.com>
2024-04-09 23:26:43 +0530

    more method added

--- src/App.jsx
+++ src/App.jsx
@@ -1,54 +1,12 @@
-import { useEffect, useState } from 'react'
 import './App.css'
-import axios from "axios"
+import GetApi from './GetApi'
 
 function App() {
- const [mydata, setMyData] = useState([])
- const [isError, setIsError] = useState("")
-
-  //note: Using Promises
-  // useEffect(() => {
-  //   axios
-  //   .get("https://jsonplaceholder.typicode.com/posts")
-  //   .then((res) => setMyData(res.data))
-  //   .catch((error) => 
-  //    setIsError(error.message)
-  //   );
-  // }, []);
-
-  // NOTE: using Async Await
-
-  
-  const getApiData = async()=>{
-    try {
-      const res = await axios.get("https://jsonplaceholder.typicode.com/posts");
-    setMyData(res.data);    
-    } catch (error) {
-      setIsError(error.message)
-    }
-    
-  };
-
-  useEffect(()=>{
-    getApiData();
-  },[]);
+   
 
   return (
     <>
-       <h1> Axios Tutorial</h1>
-
-       {isError!= "" && <h2>{isError}</h2>}
-       <div className="grid">
-
-      {mydata.slice(0,25).map((post)=>{
-        const {id, title, body} = post;
-        return <div className='card' key={id}>
-          <h2>{title.slice(0,15).toUpperCase()}</h2>
-          <p>{body.slice(0, 150)}</p>
-        </div>
-        
-      })}
-      </div>
+      <GetApi/> 
     </>
   )
 }
